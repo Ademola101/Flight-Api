d35744b199053dcc7518baab002ff1563740659d
Ademola <ademolaogunmokun@yahoo.com>
2022-06-23 16:18:56 +0100

    all flight

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,11 @@
+const Flights = require("../models/Flight")
+
 exports.example = (req, res) => {
     console.log("example")
     res.send("Flight example")
 }
 
+exports.allFlight = (req,res) => {
+    res.json(Flights.exampleModel)
+}
 
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/flightController');
 
 router.get('/', controller.example)
+router.get("/api/allFlight", controller.allFlight)
 
 module.exports = router;
 
